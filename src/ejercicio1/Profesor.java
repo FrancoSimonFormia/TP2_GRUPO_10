cb20fd55517c8be02c2370005704437e0dd4ea21
Jose Arias <jose.arias@alumnos.frgp.utn.edu.ar>
2024-08-23 21:43:13 -0300

    Agrego El encapsulamiento, constructores y metodo tostring()

--- src/ejercicio1/Profesor.java
+++ src/ejercicio1/Profesor.java
@@ -6,5 +6,40 @@ public class Profesor extends Empleado {
 	private String cargo;
 	
 	private int antiguedadDocente;
-	
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	@Override
+	public String toString() {
+	    return "Profesor Id =" + getId()
+	            + ", Nombre =" + getNombre()
+	            + ", Edad =" + getEdad()
+	            + ", Cargo " + cargo
+	            + ", AntiguedadDocente=" + antiguedadDocente + "]";
+	}
+
+	public int getAntiguedadDocente() {
+		return antiguedadDocente;
+	}
+
+	public void setAntiguedadDocente(int antiguedadDocente) {
+		this.antiguedadDocente = antiguedadDocente;
+	}
+
+	public Profesor(String cargo, int antiguedadDocente) {
+		super();
+		this.cargo = cargo;
+		this.antiguedadDocente = antiguedadDocente;
+	}
+
+	public Profesor() {
+		super();	
+	}
 }
+	
