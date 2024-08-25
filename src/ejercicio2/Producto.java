00a00e03e4ee80b004e636eac1bf9b8b7b80e86c
Jose Arias <jose.arias@alumnos.frgp.utn.edu.ar>
2024-08-25 20:32:55 -0300

    Doy inicio a la segunda actividad

---
+++ src/ejercicio2/Producto.java
@@ -0,0 +1,38 @@
+package ejercicio2;
+
+import java.sql.Date;
+
+public class Producto {
+	
+	//fecha de caducidad y número de lote
+	
+	Date fecha_de_caducidad; 
+	int numero_de_lote;
+	
+	//get y sett.
+	
+	public Date getFecha_de_caducidad() {
+		return fecha_de_caducidad;
+	}
+	public void setFecha_de_caducidad(Date fecha_de_caducidad) {
+		this.fecha_de_caducidad = fecha_de_caducidad;
+	}
+	public int getNumero_de_lote() {
+		return numero_de_lote;
+	}
+	public void setNumero_de_lote(int numero_de_lote) {
+		this.numero_de_lote = numero_de_lote;
+	}
+	
+	//constructores
+	public Producto(Date fecha_de_caducidad, int numero_de_lote) {
+		this.fecha_de_caducidad = fecha_de_caducidad;
+		this.numero_de_lote = numero_de_lote;
+	}
+
+	public Producto() {
+			
+    }
+	
+	
+}
---
+++ src/ejercicio2/ProductoFresco.java
@@ -0,0 +1,12 @@
+package ejercicio2;
+
+import java.sql.Date;
+
+public class ProductoFresco extends Producto {
+
+	//Inmformacion especifica 
+	
+	Date fecha_envasado;
+	String Pais_de_Origen;
+	
+}
