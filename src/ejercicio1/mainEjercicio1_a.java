7b9525e06cb02321d8bcf726b6f3fbce744509f5
FrancoSimonFormia <franco.formia@alumnos.frgp.utn.edu.ar>
2024-08-24 21:41:29 -0300

    implementar arraylist e iterator en main

--- src/ejercicio1/mainEjercicio1_a.java
+++ src/ejercicio1/mainEjercicio1_a.java
@@ -1,18 +1,23 @@
 package ejercicio1;
 
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class mainEjercicio1_a {
 
 	public static void main(String[] args) {
-		Profesor[] profesores = new Profesor[5];
-		profesores[0] = new Profesor("Dwight", 40,"Ayudante", 1);
-		profesores[1] = new Profesor("Jim", 35, "Docente", 5);
-		profesores[2] = new Profesor("Pam", 32, "Docente", 6);
-		profesores[3] = new Profesor("Andrew", 37, "Jefe TP", 3);
-		profesores[4] = new Profesor("Michael", 42, "Jefe de cátedra", 10);
 		
-		for (Profesor p : profesores) {
-			System.out.println(p.toString());
-		}
+		ArrayList<Profesor> profesores = new ArrayList<Profesor>();
+		
+		profesores.add(new Profesor("Dwight", 40,"Ayudante", 1));
+		profesores.add(new Profesor("Jim", 35, "Docente", 5));
+		profesores.add(new Profesor("Pam", 32, "Docente", 6));
+		profesores.add(new Profesor("Andrew", 37, "Jefe TP", 3));
+		profesores.add(new Profesor("Michael", 42, "Jefe de cátedra", 10));
+		
+		ListIterator it = profesores.listIterator();
+		
+		
 
 	}
 
