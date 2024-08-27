4560deb485eb1ab2e0936dea025343b675557ccd
FrancoSimonFormia <franco.formia@alumnos.frgp.utn.edu.ar>
2024-08-26 21:36:40 -0300

    Constructores, getters y setters en ProductosRefrigerados

--- src/ejercicio2/ProductosRefrigerados.java
+++ src/ejercicio2/ProductosRefrigerados.java
@@ -1,5 +1,30 @@
 package ejercicio2;
 
+import java.sql.Date;
+
 public class ProductosRefrigerados extends Producto {
+	
+	String codigo_supervision_alimentaria;
+
+	public String getCodigo_supervision_alimentaria() {
+		return codigo_supervision_alimentaria;
+	}
+
+	public void setCodigo_supervision_alimentaria(String codigo_supervision_alimentaria) {
+		this.codigo_supervision_alimentaria = codigo_supervision_alimentaria;
+	}
+
+	public ProductosRefrigerados() {
+		super();
+		
+	}
+
+	public ProductosRefrigerados(Date fecha_de_caducidad, int numero_de_lote,
+			String codigo_supervision_alimentaria) {
+		super(fecha_de_caducidad, numero_de_lote);
+		this.codigo_supervision_alimentaria = codigo_supervision_alimentaria;
+	}
+	
+	
 
 }
