9d0093d3cda3a3ee4841361e3e0effdf263e53e9
glogatti <logattigustavo@gmail.com>
2024-08-28 21:08:58 -0300

    atributo nombre Polideportivo, constructores y método toString()

--- src/ejercicio3/Polideportivo.java
+++ src/ejercicio3/Polideportivo.java
@@ -3,11 +3,39 @@ package ejercicio3;
 public class Polideportivo extends Edificio implements IInstalacionDeportiva {
 
 	// FALTA DESARROLLAR
+	private String nombre;
 	
+	//constructores
+	public Polideportivo() {
+		super();
+	}
+	
+	public Polideportivo(String nombre, double ancho, double largo) {
+		super(ancho, largo);
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
 	@Override
 	public int getTipoDeInstalacion() {
 		// FALTA DESARROLLAR METODO
 		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return 
+				"Polideportivo nombre: " + nombre + 
+				"| superficie: " + super.getSuperficieEdificio() + "mts cuadrados";
 	}
 
 	
