bb7a561589a7e90160d3ab3610c114c82a2b4368
joaquinnoceti <joaquinnoceti@hotmail.com>
2024-08-29 20:00:40 -0300

    main con list y listiterator

--- src/ejercicio3/mainEjercicio3.java
+++ src/ejercicio3/mainEjercicio3.java
@@ -1,9 +1,26 @@
 package ejercicio3;
+import java.util.ArrayList;
+import java.util.ListIterator;
 
-public class mainEjercicio3 {
-
-	public static void main(String[] args) {
-
-		
-	}
+public class Main {
+    public static void main(String[] args) {
+    	
+        ArrayList<Edificio> edificios = new ArrayList<>();
+        
+        // Polideportivos
+        edificios.add(new Polideportivo("Hector Fillopski", "Campana", 50, 100));
+        edificios.add(new Polideportivo("Club Social y Deportivo San Martín", "Burzaco", 60, 120));
+        edificios.add(new Polideportivo("Polideportivo Club Laferrere", "Gregorio de Laferrere", 70, 140));
+        
+        // Oficinas
+        edificios.add(new Oficinas(40, 55, 10, 3));
+        edificios.add(new Oficinas(30, 60, 20, 5));
+        
+        // ListIterator y recorrida de coleccion
+        ListIterator<Edificio> it = edificios.listIterator();
+        while (it.hasNext()) {
+            Edificio edificio = it.next();
+            System.out.println(edificio.toString());
+        }
+    }
 }
